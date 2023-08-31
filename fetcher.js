202fa4d7577e0f72ddf3f465c0d506f49615dca8
RomelDonovan <RomelDonovan@live.ca>
2023-08-31 22:16:48 +0000

    Implemnt a node app called fetcher.js

---
+++ fetcher.js
@@ -0,0 +1,21 @@
+const request = require("request");
+const fs = require("fs");
+const { error } = require("console");
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log("error: ", error); // Print the error if one occurred
+    process.exit;
+  };
+
+  fs.writeFile(filePath, body, error => {
+    if (error) {
+      console.log("error: ", error)
+      return;
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to ${filePath}`);
+  });
+});
